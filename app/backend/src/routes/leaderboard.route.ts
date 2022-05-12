4b7b3002e4c387ec8c6cc6c3e1e29e8305c4bfd3
João V. Dias <diasvictorj@gmail.com>
2022-05-12 11:20:50 -0300

    Leadeboard atualizado

--- app/backend/src/routes/leaderboard.route.ts
+++ app/backend/src/routes/leaderboard.route.ts
@@ -3,7 +3,15 @@ import LeaderboardController from '../controllers/leaderboard.controller';
 
 const leaderboardsRouter = Router();
 
+leaderboardsRouter.get('/', async (req: Request, res: Response) => {
+  await LeaderboardController.getLeaderboard(req, res);
+});
+
 leaderboardsRouter.get('/home', async (req: Request, res: Response) => {
+  await LeaderboardController.getLeaderboard(req, res);
+});
+
+leaderboardsRouter.get('/away', async (req: Request, res: Response) => {
   await LeaderboardController.getLeaderboard(req, res);
 });
 
